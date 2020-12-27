f2d3b26de81cb43350d0bf917f6fdf898391c4b6
Nick Knox <nick@knox.codes>
2020-12-26 23:50:22 -0800

    removed templating

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,8 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Link } from 'react-scroll';
-import PortfolioContext from '../../context/context';
-import GithubButtons from '../GithubButtons/GithubButtons';
-
-import { githubButtons } from '../../mock/data';
 
 const Footer = () => {
-  const { footer } = useContext(PortfolioContext);
-  const { networks } = footer;
-  const { isEnabled } = githubButtons;
-
   return (
     <footer className="footer navbar-static-bottom">
       <Container>
@@ -20,21 +12,14 @@ const Footer = () => {
           </Link>
         </span>
         <div className="social-links">
-          {networks &&
-            networks.map((network) => {
-              const { id, name, url } = network;
-              return (
-                <a
-                  key={id}
-                  href={url || 'https://github.com/cobidev/gatsby-simplefolio'}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  aria-label={name}
-                >
-                  <i className={`fa fa-${name || 'refresh'} fa-inverse`} />
-                </a>
-              );
-            })}
+          <a
+            href="https://www.linkedin.com/company/cyber-ucsd/"
+            rel="noopener noreferrer"
+            target="_blank"
+            aria-label="linkedin"
+          >
+            <i className="fa fa-linkedin fa-inverse" />
+          </a>
         </div>
         <hr />
         <p className="footer__text">
@@ -43,8 +28,6 @@ const Footer = () => {
             Jacobo Martínez
           </a>
         </p>
-
-        {isEnabled && <GithubButtons />}
       </Container>
     </footer>
   );
